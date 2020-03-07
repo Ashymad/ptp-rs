fa7f02010de57036367fd443215736ab17b25865
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2020-03-07 02:54:19 +0100

    Join multicast group

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-extern crate signal_hook;
-
 use mio::{Events, Poll, Interest, Token};
 use mio::net::UdpSocket;
 
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 
-use std::net::{SocketAddr};
+use std::net::{SocketAddr, Ipv4Addr};
 
 fn main() {
     let sigint = Arc::new(AtomicBool::new(false));
     signal_hook::flag::register(signal_hook::SIGINT, Arc::clone(&sigint)).unwrap();
 
-    let addr: SocketAddr = "0.0.0.0:319".parse().unwrap();
-    let mut socket = UdpSocket::bind(addr).unwrap();
+    let addr_str = "192.168.0.10";
+    let multicast_addr_str = "224.0.1.129";
+    let port_str = "319";
+    let soc_addr: SocketAddr = format!("{}:{}", addr_str, port_str).parse().unwrap();
+    let addr: Ipv4Addr = addr_str.parse().unwrap();
+    let multicast_addr: Ipv4Addr = multicast_addr_str.parse().unwrap();
+    let mut socket = UdpSocket::bind(soc_addr).unwrap();
+    socket.join_multicast_v4(&multicast_addr, &addr).unwrap();
 
     let mut poll = Poll::new().unwrap();
     let mut events = Events::with_capacity(128);
 
     poll.registry().register(&mut socket, Token(0), Interest::READABLE).unwrap();
 
-    println!("Listening on: {}", addr);
+    println!("Listening on: {}, multicast group: {}", soc_addr, multicast_addr);
     while !sigint.load(Ordering::Relaxed) {
         if let Err(err) = poll.poll(&mut events, None) {
             if err.kind() == std::io::ErrorKind::Interrupted {
@@ -39,4 +43,6 @@ fn main() {
             }
         }
     }
+
+    socket.leave_multicast_v4(&multicast_addr, &addr).unwrap();
 }
