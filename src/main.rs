b8ec5e7d4ec782263ce55df4ff6419f6c7dad921
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2020-03-07 03:31:31 +0100

    And now it actually receives the messages

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,25 @@ use mio::net::UdpSocket;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 
-use std::net::{SocketAddr, Ipv4Addr};
-
 fn main() {
     let sigint = Arc::new(AtomicBool::new(false));
     signal_hook::flag::register(signal_hook::SIGINT, Arc::clone(&sigint)).unwrap();
 
-    let addr_str = "192.168.0.10";
-    let multicast_addr_str = "224.0.1.129";
-    let port_str = "319";
-    let soc_addr: SocketAddr = format!("{}:{}", addr_str, port_str).parse().unwrap();
-    let addr: Ipv4Addr = addr_str.parse().unwrap();
-    let multicast_addr: Ipv4Addr = multicast_addr_str.parse().unwrap();
-    let mut socket = UdpSocket::bind(soc_addr).unwrap();
-    socket.join_multicast_v4(&multicast_addr, &addr).unwrap();
-
+    let iface_addr = "192.168.0.10".parse().unwrap();
+    let multicast_addr = "224.0.1.129".parse().unwrap();
+    let bind_addr_event = "0.0.0.0:319".parse().unwrap();
+    let bind_addr_general = "0.0.0.0:320".parse().unwrap();
+    let mut socket_general = UdpSocket::bind(bind_addr_general).unwrap();
+    let mut socket_event = UdpSocket::bind(bind_addr_event).unwrap();
+    socket_general.join_multicast_v4(&multicast_addr, &iface_addr).unwrap();
+    socket_event.join_multicast_v4(&multicast_addr, &iface_addr).unwrap();
     let mut poll = Poll::new().unwrap();
     let mut events = Events::with_capacity(128);
 
-    poll.registry().register(&mut socket, Token(0), Interest::READABLE).unwrap();
+    poll.registry().register(&mut socket_event, Token(0), Interest::READABLE).unwrap();
+    poll.registry().register(&mut socket_general, Token(1), Interest::READABLE).unwrap();
 
-    println!("Listening on: {}, multicast group: {}", soc_addr, multicast_addr);
+    println!("Listening on: General: {}, Event: {}, Multicast: {}", bind_addr_general, bind_addr_event, multicast_addr);
     while !sigint.load(Ordering::Relaxed) {
         if let Err(err) = poll.poll(&mut events, None) {
             if err.kind() == std::io::ErrorKind::Interrupted {
@@ -35,14 +33,20 @@ fn main() {
         }
 
         for event in &events {
-            if event.token() == Token(0) && event.is_readable() {
-                let mut buf = [0u8; 34];
-                let (number_of_bytes, src_addr) = socket.recv_from(&mut buf)
-                                    .expect("Didn't receive data");
-                println!("Received ptp message from: {}, of type: {}, length {} bytes", src_addr, buf[0] & 0x0F, number_of_bytes);
+            if event.is_readable() {
+            let mut buf = [0u8; 34];
+            let (number_of_bytes, src_addr) = if event.token() == Token(0) {
+                socket_event.recv_from(&mut buf).expect("Didn't receive data")
+            } else if event.token() == Token(1) {
+                socket_general.recv_from(&mut buf).expect("Didn't receive data")
+            } else {
+                panic!("Unknown token: {:?}", event.token())
+            };
+            println!("Received ptp message from: {}, of type: {}, length {} bytes", src_addr, buf[0] & 0x0F, number_of_bytes);
             }
         }
     }
 
-    socket.leave_multicast_v4(&multicast_addr, &addr).unwrap();
+    socket_general.leave_multicast_v4(&multicast_addr, &iface_addr).unwrap();
+    socket_event.leave_multicast_v4(&multicast_addr, &iface_addr).unwrap();
 }
